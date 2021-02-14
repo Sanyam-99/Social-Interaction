f3a87b10127487bae9a826a6b08fc8ccea7a21a3
Sanyam-99 <79032870+Sanyam-99@users.noreply.github.com>
2021-02-14 09:42:40 +0530

    hello

---
+++ app/src/main/java/Model/Notification.java
@@ -0,0 +1,49 @@
+package Model;
+
+public class Notification {
+    private String userid;
+    private String postid;
+    private String text;
+    private boolean isPost;
+    public Notification(){}
+
+    public Notification(String userid, String postid, String text, boolean isPost) {
+        this.userid = userid;
+        this.postid = postid;
+        this.text = text;
+        this.isPost = isPost;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPostid() {
+        return postid;
+    }
+
+    public void setPostid(String postid) {
+        this.postid = postid;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isPost() {
+        return isPost;
+    }
+
+    public void setPost(boolean post) {
+        isPost = post;
+    }
+}
+
